a460ab2b74540f5a1c6a815b9bba7f9d5ee6e79e
Abayomi Aremo <8955yomi@gmail.com>
2022-12-19 15:19:11 -0700

    removed repitions and commented for easy understanding

--- main.js
+++ main.js
@@ -8,35 +8,27 @@ let highscore = document.querySelector('.highscore')
 let againbtn = document.querySelector(".btnagain")
 let scorepoints = 20
 
-againbtn.addEventListener('click',restart)
-function restart(){
-    console.log('active')
-    score.textContent = 20
-    message.textContent = "Start guessing..."
-    correctnum = Math.floor(Math.random()*20)+1
-    number.textContent = '?'
-    guess.value = " "
-    document.querySelector('body').style.backgroundColor = "#222"
-    number.style.width = '15rem'
+
+//check button and its function
+check.addEventListener('click',gamelogic)
+
+//message function
+function displaymessage(text){
+    message.textContent = text
 }
 
-
-
-check.addEventListener('click',gamelogic)
+//game logic
 function gamelogic(e){
     localStorage.setItem("highscore",highscore.textContent)
     scorepoints--
-    
-    if(guess.value > correctnum ){
-        score.textContent = scorepoints
-        message.textContent = "👇🏻 go lower"
-        guess.value = " "
-    }else if(guess.value < correctnum){
-        message.textContent = "👆🏻 go higher"
+    if(guess.value == 0){
+        displaymessage('📛 No number')
+    }else if(guess.value>correctnum || guess.value<correctnum){
+        guess.value>correctnum? displaymessage("👇🏻 go lower"):displaymessage("👆🏻 go higher")
         score.textContent = scorepoints
         guess.value = " "
     }else if(guess.value == correctnum){
-        message.textContent = "🏆 you win"
+        displaymessage("🏆 you win")
         number.textContent = correctnum
         document.querySelector('body').style.backgroundColor = "#60b347"
         number.style.width = "30rem"
@@ -45,16 +37,26 @@ function gamelogic(e){
         if(scorepoints >localStorage.getItem("highscore") ){
             localStorage.setItem("highscore",scorepoints)
             highscore.textContent= localStorage.getItem("highscore")
-}
+        }
     }
 
     if(scorepoints == 0){
-        message.textContent = "📛 you lose"
+        displaymessage("📛 you lose")
         number.textContent = correctnum
         check.disabled = true
     }
-
-
-   
     e.preventDefault()
+}
+
+//againbutton reload logic
+againbtn.addEventListener('click',restart)
+function restart(){
+    console.log('active')
+    score.textContent = 20
+    displaymessage("Start guessing...")
+    correctnum = Math.floor(Math.random()*20)+1
+    number.textContent = '?'
+    guess.value = " "
+    document.querySelector('body').style.backgroundColor = "#222"
+    number.style.width = '15rem'
 }
